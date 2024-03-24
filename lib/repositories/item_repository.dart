3c2293663c45ed294fcc4bfe8d44bd183bb98f11
numen111104 <numannasyarmz11@gmail.com>
2024-03-24 09:47:44 +0900

    feat: added 2 function pickUp() and addOrUpdateItem()

--- lib/repositories/item_repository.dart
+++ lib/repositories/item_repository.dart
@@ -1,14 +1,29 @@
 import 'package:go_lance/entitles/item.dart';
+import 'package:go_lance/entitles/user.dart';
 import 'package:go_lance/objectbox.g.dart';
 
-class ItemRepository{
+class ItemRepository {
   final Store store;
   ItemRepository({required this.store});
 
   int addOrUpdateItem(Item item) {
     return store.box<Item>().put(item);
   }
+
   Stream<Query<Item>> getQueryStream(Condition<Item>? condition) {
     return store.box<Item>().query(condition).watch(triggerImmediately: true);
+  }
+
+  int requestItem({required Item item, required User user}) {
+    item.driver.target = user;
+    return addOrUpdateItem(item);
+  }
+
+  int pickUpItem({required Item item}) {
+    if (item.driver.target != null) {
+      item.isPickedUp = true;
+      return addOrUpdateItem(item);
+    }
+    return 0;
   }
 }
