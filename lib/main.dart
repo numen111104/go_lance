a760aca0dd9de7e7c4d9335390a6f14e66b1aeb7
numen111104 <numannasyarmz11@gmail.com>
2024-03-24 09:38:51 +0900

    fix: Sync the database with other device

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:go_lance/presentation/pages/login_page.dart';
 import 'package:go_lance/presentation/pages/welcome_page.dart';
@@ -6,16 +8,29 @@ import 'package:go_lance/repositories/object_box.dart';
 import 'objectbox.g.dart';
 
 late Store store;
+SyncClient? Sclient;
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   store = (await ObjectBox.create()).store;
+
+  if(Sync.isAvailable()){
+    Sclient = Sync.client(store, Platform.isAndroid ? "ws://10.0.2.2:9999" : "ws://127.0.0.1:9999", SyncCredentials.none());
+
+    Sclient?.start();
+  }
+
   runApp(const MainApp());
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends StatefulWidget {
   const MainApp({super.key});
 
+  @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -27,5 +42,13 @@ class MainApp extends StatelessWidget {
         '/login' :(context) => LoginPage(),
       },
       );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    store.close();
+    Sclient?.stop();
+    Sclient?.close();
   }
 }
